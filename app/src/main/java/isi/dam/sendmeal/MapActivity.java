1f9fe9df439384c081b0bc4d4e635f4dfbe76971
exesanchz <exequiel_2080@hotmail.com>
2021-01-15 16:41:12 -0300

    Implementacion de mapas (hasta act 6)

--- app/src/main/java/isi/dam/sendmeal/MapActivity.java
+++ app/src/main/java/isi/dam/sendmeal/MapActivity.java
@@ -29,11 +29,17 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.SphericalUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Random;
 
 import model.CuentaBancaria;
 import model.Tarjeta;
@@ -42,12 +48,18 @@ import model.Usuario;
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private LocationManager locationManager;
+    private ArrayList<Marker> marcadores ;
+    private ArrayList<Polyline> recorridos ;
+    private LatLng ubicacion;
+    private LatLng ubicacionSendMeal;
     Button confirmar_ubicacion;
     TextView latitud, longitud;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        marcadores = new ArrayList<Marker>();
+        recorridos = new ArrayList<Polyline>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -62,11 +74,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                String latitud_str =  "";
-                String longitud_str =  "";
-                intent.putExtra("latitud", latitud_str);
-                intent.putExtra("longitud", longitud_str);
-                //fijarse si vamos a mandar latitud y longitud, o directamente armamos la dirección
+                intent.putExtra("latitud", ubicacion.latitude);
+                intent.putExtra("longitud", ubicacion.longitude);
+
                 setResult(RESULT_OK, intent);
                 finish();
 
@@ -108,10 +118,74 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         LatLng utn = new LatLng(-31.6168, -60.6752);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(utn,15));
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                latitud.setText("Latitud: "+marker.getPosition().latitude);
+                longitud.setText("Longitud: "+marker.getPosition().longitude);
+                ubicacion = marker.getPosition();
+                return false;
+            }
+        });
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
 
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker)  {
+                latitud.setText("Latitud: "+marker.getPosition().latitude);
+                longitud.setText("Longitud: "+marker.getPosition().longitude);
+                if (!recorridos.isEmpty()){
+                    recorridos.get(0).remove();
+                    recorridos.clear();
+                }
+                //Generamos el recorrido entre la ubicación Send Meal y la del cliente
+                PolylineOptions polylineOptions = new PolylineOptions()
+                        .color(0x66FF0000)
+                        .add(ubicacionSendMeal)
+                        .add(marker.getPosition());
+
+                Polyline linea = mMap.addPolyline(polylineOptions);
+                recorridos.add(linea);
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                latitud.setText("Latitud: "+marker.getPosition().latitude);
+                longitud.setText("Longitud: "+marker.getPosition().longitude);
+                ubicacion = marker.getPosition();
+
+                if (!recorridos.isEmpty()){
+                    recorridos.get(0).remove();
+                    recorridos.clear();
+                }
+                //Generamos el recorrido entre la ubicación Send Meal y la del cliente
+                PolylineOptions polylineOptions = new PolylineOptions()
+                        .color(0x66FF0000)
+                        .add(ubicacionSendMeal)
+                        .add(ubicacion);
+
+                Polyline linea = mMap.addPolyline(polylineOptions);
+                recorridos.add(linea);
+            }
+        });
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
+               if (!marcadores.isEmpty()){
+                 marcadores.get(0).remove();
+                 marcadores.clear();
+                }
+                if (!recorridos.isEmpty()){
+                    recorridos.get(0).remove();
+                    recorridos.clear();
+                }
+                latitud.setText("Latitud: "+latLng.latitude);
+                longitud.setText("Longitud: "+latLng.longitude);
+                ubicacion = latLng;
                 MarkerOptions marcador = new MarkerOptions()
                         .alpha(0.5f)
                         .position(latLng)
@@ -119,13 +193,47 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                         .title("Mi ubicación")
                         .snippet("Subtitulo")
                         .icon(BitmapDescriptorFactory .defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                mMap.addMarker(marcador);
+               Marker M =  mMap.addMarker(marcador);
+                marcadores.add(M);
+
+                //Generamos el recorrido entre la ubicación Send Meal y la del cliente
+                PolylineOptions polylineOptions = new PolylineOptions()
+                        .color(0x66FF0000)
+                        .add(ubicacionSendMeal)
+                        .add(ubicacion);
+
+                Polyline linea = mMap.addPolyline(polylineOptions);
+                recorridos.add(linea);
             }
         });
+        //Generamos punto aleatorio para el local, aproximado por defecto a la facultad
 
-        System.out.println("LLEGO MAPA");
+        LatLng posicionOriginal = new LatLng(-31.61280,-60.69581); //sobre calle aristobulo, cercano a la UTN
+        Random r = new Random();
+
+        // Una direccion aleatoria de 0 a 359 grados
+        int direccionRandomEnGrados = r.nextInt(360);
+
+        // Una distancia aleatoria de 100 a 1000 metros
+        int distanciaMinima = 100;
+        int distanciaMaxima = 1000;
+        int distanciaRandomEnMetros = r.nextInt(distanciaMaxima - distanciaMinima) + distanciaMinima;
+
+        ubicacionSendMeal = SphericalUtil.computeOffset(
+                posicionOriginal,
+                distanciaRandomEnMetros,
+                direccionRandomEnGrados
+        );
+        MarkerOptions marcadorSendMeal = new MarkerOptions()
+                .alpha(0.5f)
+                .position(ubicacionSendMeal)
+                .draggable(true)
+                .title("SendMeal")
+                .snippet("Comidas Rápidas")
+                .icon(BitmapDescriptorFactory .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+        mMap.addMarker(marcadorSendMeal);
+
     }
-
 
 
 }
